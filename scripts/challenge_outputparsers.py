d2a0e70b663f79d3bc7a64571d839891856efb5e
estudart <erico.studart@hashdex.com>
2025-04-16 18:44:25 -0300

    fix: class name for the review

--- scripts/challenge_outputparsers.py
+++ scripts/challenge_outputparsers.py
@@ -6,7 +6,7 @@ from extensions import chat
 
 
 
-class Piada(BaseModel):
+class Review(BaseModel):
     descricao: str = Field(description="Dê uma breve descrição em relação ao produto citado no texto")
     cliente_satisfeito_entrega: bool = Field(description="Diga se o cliente ficou satisfeito com a entrega no formato: true ou false")
     cliente_satisfeito_produto: bool = Field(description="Diga se o cliente ficou satisfeito com o produto no formato: true ou false")
@@ -14,6 +14,6 @@ class Piada(BaseModel):
     nota_satisfacao: Optional[int] = Field(description="Retorne uma nota de 0 a 10 para a satisfação geral do cliente em relação a compra")
 
 
-llm_estruturada = chat.with_structured_output(Piada)
+llm_estruturada = chat.with_structured_output(Review)
 resposta = llm_estruturada.invoke(client_review_1)
 print(resposta)
