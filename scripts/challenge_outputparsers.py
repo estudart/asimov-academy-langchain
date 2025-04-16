fa9700a4b8c46ff1721b416737c565655b1d3792
estudart <erico.studart@hashdex.com>
2025-04-16 18:41:14 -0300

    feat: outputparser logic file

---
+++ scripts/challenge_outputparsers.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+from client_reviews import client_review_1
+from extensions import chat
+
+
+
+class Piada(BaseModel):
+    descricao: str = Field(description="Dê uma breve descrição em relação ao produto citado no texto")
+    cliente_satisfeito_entrega: bool = Field(description="Diga se o cliente ficou satisfeito com a entrega no formato: true ou false")
+    cliente_satisfeito_produto: bool = Field(description="Diga se o cliente ficou satisfeito com o produto no formato: true ou false")
+    nota_atendimento: Optional[int] = Field(description="Retorne uma nota de 0 a 10 para o atendimento fornecido pela loja")
+    nota_satisfacao: Optional[int] = Field(description="Retorne uma nota de 0 a 10 para a satisfação geral do cliente em relação a compra")
+
+
+llm_estruturada = chat.with_structured_output(Piada)
+resposta = llm_estruturada.invoke(client_review_1)
+print(resposta)
