48204b13f572d37621b08055a2ac2f4cce8323a8
estudart <erico.studart@hashdex.com>
2025-04-16 18:41:26 -0300

    feat: cliente reviews file

---
+++ scripts/client_reviews.py
@@ -0,0 +1,11 @@
+client_review_1 = """
+Este soprador de folhas é bastante incrível. Ele tem 
+quatro configurações: sopro de vela, brisa suave, cidade ventosa 
+e tornado. Chegou em dois dias, bem a tempo para o presente de 
+aniversário da minha esposa. Acho que minha esposa gostou tanto 
+que ficou sem palavras. Até agora, fui o único a usá-lo, e tenho 
+usado em todas as manhãs alternadas para limpar as folhas do 
+nosso gramado. É um pouco mais caro do que os outros sopradores 
+de folhas disponíveis no mercado, mas acho que vale a pena pelas 
+características extras.
+"""
